8e6c7a485f9d7a658da5d03a8fae1ac32a7e87c0
DimaMarkow <dimamarkow@gmail.com>
2022-11-30 18:50:40 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './css/styles.css';
 
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+fetch(`https://restcountries.com/v3.1/name/peru`)
+  .then(response => {
+    return response.json();
+  })
+  .then(countryes => {
+    console.log(countryes);
+    countryes.forEach(countrye => {
+      console.log(countrye.name.official);
+      console.log(countrye.capital);
+      console.log(countrye.population);
+      console.log(countrye.flags.svg);
+      console.log(countrye.languages);
+    });
+  });
+
+const refs = {
+  form: document.querySelector(`#search-box`),
+};
+
+refs.form.addEventListener(`input`, onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  _.debounce(console.log(event.target.value), 3000);
+  //   console.log(event.target.value);
+}
